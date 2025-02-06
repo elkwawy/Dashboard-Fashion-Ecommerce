fe1a3269fae701e1b3ab48eb805e94ff83707e87
unknown <ama991969@gmail.com>
2025-02-06 15:26:28 +0200

    delete all files

--- src/Auth/getAuthHeader.js
+++
@@ -1,8 +0,0 @@
-import Cookies from "js-cookie";
-
-export const getAuthHeader = () => {
-  const token = Cookies.get("token");
-  if (token)
-    return { headers: { Authorization: JSON.parse(token) } };
-  return null ;
-};
--- src/utils/Loader.jsx
+++
@@ -1,7 +0,0 @@
-const Loader = () => {
-    return (
-        <img src="/public/loadingSpinner.svg" alt="Loading..." className="w-10" />
-    )
-}
-
-export default Loader
