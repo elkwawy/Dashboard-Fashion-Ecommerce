4739808938f2eb4a15695723d8d6ec37b0d8745e
unknown <ama991969@gmail.com>
2025-02-16 02:11:34 +0200

    Updata Project

--- src/pages/admin/adminList/components/UpdateModal.jsx
+++ src/pages/admin/adminList/components/UpdateModal.jsx
@@ -83,7 +83,7 @@ const UpdateModal = memo(({admin,currUser,onClose}) => {
 
     return (
         <div className=" overflow-y-auto overflow-x-hidden fixed top-0 left-0 flex  z-[110]  justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
-            <div className="flex flex-col gap-2 bg-white w-3/4 z-[110] rounded-md  p-8  ">
+            <div className="flex flex-col gap-2 bg-white w-[64.5%] z-[110] rounded-md  p-8  ">
                 <button onClick={onClose} type="button" className="ml-auto trans end-2.5 text-gray-400 bg-transparent hover:bg-red-100  rounded-md w-8 h-8 ms-auto inline-flex justify-center items-center " data-modal-hide="popup-modal">
                     <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
                         <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
