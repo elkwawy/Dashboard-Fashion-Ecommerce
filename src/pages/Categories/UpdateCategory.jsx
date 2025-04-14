de851e263565270314ef6d468337b58eba307468
unknown <ama991969@gmail.com>
2025-04-14 14:23:49 +0200

    Updata Project

--- src/pages/Categories/UpdateCategory.jsx
+++ src/pages/Categories/UpdateCategory.jsx
@@ -7,7 +7,7 @@ import {
 import { useLocation, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import toast from "react-hot-toast";
-import addavatar from "../../assets/avater/update-cuate.svg";
+import addavatar from "../../assets/avater/Update-cuate.svg";
 
 export default function UpdateCategory() {
   const location = useLocation()
